7a0b0d9328943511a5939eaa6d658e721c6842e7
vincentes <thevincentes@gmail.com>
2022-08-12 01:15:08 -0300

    Remove console logs

--- middlewares/validateMAC.js
+++ middlewares/validateMAC.js
@@ -12,19 +12,13 @@ const checkIfHasMAC = async (req, res, next) => {
   const { ServerDateTime } = req.body;
   const key1 = `${process.env.API_KEY}:${RequestID}:${ServerDateTime}`;
   const key2 = `${process.env.API_KEY}:${jsonStr}`;
-  console.log('key2', key2);
 
   const key2MD5 = crypto.createHash('md5').update(key2).digest('hex');
-
-  console.log('key2md5', key2MD5);
 
   const MAC = crypto
     .createHash('md5')
     .update(key1 + key2MD5)
     .digest('hex');
-
-  console.log('mac', MAC);
-  console.log('code', code);
 
   if (code === MAC) {
     next();
