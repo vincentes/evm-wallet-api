bd32076271366ca9eb3aca4c345f938f10e6fca1
vincentes <thevincentes@gmail.com>
2022-10-26 13:29:28 -0300

    Remove config.json from .gitignore

---
+++ config/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
+module.exports = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+};
