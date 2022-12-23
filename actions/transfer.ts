c04a9772812967f38bce0a2807d11b86af87e8b4
vincentes <thevincentes@gmail.com>
2022-12-23 01:23:43 -0300

    Use getWalletInfo

--- actions/transfer.ts
+++ actions/transfer.ts
@@ -2,31 +2,19 @@ import { Network, TokenType } from "../constants/constants";
 import { Priority } from "../enum/priority";
 import { getConfiguredWallet, isConfiguredWallet, transform } from "../utils/storage";
 import { TronHotWallet } from "./wallet-create-tron";
+import { getWalletInfo } from "./wallet-info";
 
 const eth = require('./transfer-eth');
 const tron = require('./transfer-tron');
 
 export async function transfer(userId: string, network: Network, tokenType: TokenType, fromAddress: string, targetAddress: string, amount: string, native: boolean, priority?: Priority) {
     if (network === Network.TRC20) {
-        const thw = new TronHotWallet();
-
-        const exists = await thw.exists(fromAddress);
-        let wallet;
-
-        if (exists) {
-            wallet = await thw.getHotWallet(fromAddress);
-        } else {
-            if (isConfiguredWallet(fromAddress)) {
-                wallet = getConfiguredWallet(fromAddress);
-            } else {
-                throw new Error("Source Wallet does not exist in the database.")
-            }
-        }
+        let wallet = await getWalletInfo(userId, tokenType, network, fromAddress);
 
         if (native) {
-            return await tron.transferNative(tokenType, targetAddress, amount, wallet.privateKey);
+            return await tron.transferNative(tokenType, targetAddress, amount, wallet["PrivateKey"]);
         } else {
-            return await tron.transfer(tokenType, targetAddress, amount, wallet.privateKey);
+            return await tron.transfer(tokenType, targetAddress, amount, wallet["PrivateKey"]);
         }
 
     } else {
