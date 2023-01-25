04ad43bafd50076f15c81c21d3c18bc7a9149550
vincentes <thevincentes@gmail.com>
2023-01-24 22:34:15 -0300

    Make gasFee always query with "1" as amount

--- actions/gas-price-eth.ts
+++ actions/gas-price-eth.ts
@@ -3,52 +3,52 @@ import { transform } from "../utils/transform";
 import { getWeb3 } from "./eth/init";
 
 
-const minABI : any = [
-    {
-        constant: false,
-        inputs: [
-          {
-            name: "_to",
-            type: "address"
-          },
-          {
-            name: "_value",
-            type: "uint256"
-          }
-        ],
-        name: "transfer",
-        outputs: [
-          {
-            name: "",
-            type: "bool"
-          }
-        ],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function"
+const minABI: any = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_to",
+        type: "address"
+      },
+      {
+        name: "_value",
+        type: "uint256"
       }
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  }
 ];
 
-export async function getGasPrice(network: Network, tokenType : TokenType, targetAddress : string, amount : string, privateKey : string) {
-    const web3 = getWeb3(network);
-    let tokenAddress;
-    try {
-        tokenAddress = transform(network, tokenType);
-    } catch (error) {
-      return {};
-    }
-
-    const contract = new web3.eth.Contract(minABI, tokenAddress);
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-    const tx = contract.methods.transfer(targetAddress, amount);
-  
-    const gas = await tx.estimateGas({ from: account.address });
-    const gasPrice = await web3.eth.getGasPrice();
-    const cost = web3.utils.toBN(gasPrice).mul(web3.utils.toBN(gas));
-
-    return {
-      Low: web3.utils.fromWei(cost.muln(0.8)),
-      Medium: web3.utils.fromWei(cost),
-      High: web3.utils.fromWei(cost.muln(1.2))
-    };
+export async function getGasPrice(network: Network, tokenType: TokenType, targetAddress: string, amount: string, privateKey: string) {
+  const web3 = getWeb3(network);
+  let tokenAddress;
+  try {
+    tokenAddress = transform(network, tokenType);
+  } catch (error) {
+    return {};
   }
+
+  const contract = new web3.eth.Contract(minABI, tokenAddress);
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  const tx = contract.methods.transfer(targetAddress, "1");
+
+  const gas = await tx.estimateGas({ from: account.address });
+  const gasPrice = await web3.eth.getGasPrice();
+  const cost = web3.utils.toBN(gasPrice).mul(web3.utils.toBN(gas));
+
+  return {
+    Low: web3.utils.fromWei(cost.muln(0.8)),
+    Medium: web3.utils.fromWei(cost),
+    High: web3.utils.fromWei(cost.muln(1.2))
+  };
+}
--- app.ts
+++ app.ts
@@ -2,9 +2,11 @@ const io = require('@pm2/io')
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressip = require('express-ip');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
